39c9022bc523404ee511cab5d5a02c9986ef76b1
QHdld <soj041006@gmail.com>
2024-04-12 16:21:23 +0900

    post list test done

--- blog/urls.py
+++ blog/urls.py
@@ -17,5 +17,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('',views.PostList.as_view()),
 ]
